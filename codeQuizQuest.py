f2c7642c759d7c2dedcd7c59c5770f7b26aae6aa
PerryMS23 <126625740+PerryMS23@users.noreply.github.com>
2023-04-22 21:06:19 +0000

    Adding source data for the quizz

--- codeQuizQuest.py
+++ codeQuizQuest.py
@@ -1,2 +1,15 @@
-Tally = 0
-print(Tally)
+tally = 0
+while True:
+    d1a = input("What colour is an apple?: \n A) Red. B) Blue. C) Purple. [A/B/C]? : ").upper()
+    
+    if d1a == "A":
+        print("A is correct!")
+        tally = tally + 1
+        print (tally)
+        break
+    elif d1a != "A":
+        print("That is incorrect!")
+        tally = tally
+        break
+   
+    
