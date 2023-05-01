4a175cfab4b9ec41e61a2ad9cacfcd6d8e60a8da
PerryMS23 <126625740+PerryMS23@users.noreply.github.com>
2023-05-02 10:26:36 +1200

    update
    wedfiurwdfr

--- codeQuizQuest.py
+++ codeQuizQuest.py
@@ -1,16 +1,15 @@
-
-SC = 0
+thisdict = {
+    "ID": 1,
+    "Q": "What colour is an apple?",
+    "Ans": "A"
+}
+x = thisdict["Q"]
+y = thisdict["Ans"]
 while True:
-    d1a = input("What colour is an apple?: \n A) Red. B) Blue. C) Purple. [A/B/C]? : ").upper()
-    if d1a == "A":
-        print("A is correct!")
-        SC = SC + 1
-        print (SC)
+    d1a = input(x + "\n A) Red. B) Blue. C) Purple. [A/B/C]? : ").upper()
+    if d1a == y:
+        print(y + " is correct!")
         break
-    elif d1a != "A":
-        print("That is incorrect!")
-        print (SC)
-        break
-  
-        
-        
+    elif d1a != y:
+        print ("that is incorrect!")
+        break   
