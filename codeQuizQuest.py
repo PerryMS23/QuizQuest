f567fce35e9d3050e24c4c7dc44351b64d3a6f47
PerryMS23 <126625740+PerryMS23@users.noreply.github.com>
2023-06-25 13:26:43 +1200

    Update codeQuizQuest.py
    added an "INCORRECT" counter.

--- codeQuizQuest.py
+++ codeQuizQuest.py
@@ -1,4 +1,5 @@
 myvar = 0
+myvartwo = 0
 QUESTIONS = [
     ("What is 9+10?  [A]19.  [B]21.  [C]7.", "A"),
     ("What colour is an apple?  [A]Purple.  [B]Red.  [C]Blue.", "B"),
@@ -12,25 +13,28 @@ QUESTIONSTWO = [
     ("What is 9x7?  [A]19.  [B]63.  [C]7.", "B"),
     ("How many letters are in the english alphabet?  [A]25.  [B]32.  [C]26.", "C"),
     ("30 Days of September, April June and?  [A]November.  [B]July.  [C]December.", "A"),
-    ("What day is only on a leap year?  [A]Feb29th.  [B]May32nd.  [C]Jun31st.", "A"),
+    ("What day is only on a leap year?  [A]Feb29th.  [B]May32nd.  [C]Jun31st.", "B"),
     ("Whats heavier; 1kg of steel, or 1kg of feathers?  [A]Feathers.  [B]Steel.  [C]They're both the same.", "C"),
     ("What are the three primary colours of light?  [A]Red, Green, Blue.  [B]Cyan, Magenta, Yellow.  [C]Red, Yellow, Blue.", "A"),
     ("How many elements are in the periodic table?  [A]112.  [B]118.  [C]147.", "B"),
     ("How many native flightless birds does New Zealand have?  [A]17.  [B]16.  [C]21.", "B")
 ]
-#Correct Answers: B, C, A, A, C, A, B, B.
+#Correct Answers: B, C, A, B, C, A, B, B.
 for question, correct_answer in QUESTIONS:
     answer = input(f"{question} [A/B/C] ").upper()
     if answer == correct_answer:
         print("Correct!")
         myvar = myvar + 1
-        print("SCORE:")
+        print("CORRECT:")
         print(myvar)
+        print("INCORRECT:")
+        print(myvartwo)
     else:
         print(f"The answer is {correct_answer!r}, not {answer!r}")
-        print("SCORE:")
+        myvartwo = myvartwo + 1
+        print("CORRECT:")
         print(myvar)
+        print("INCORRECT:")
+        print(myvartwo)
     if myvar == 4:
-        QUESTIONS.extend(QUESTIONSTWO)
-        
-        #notes
+        QUESTIONS.extend(QUESTIONSTWO) 
